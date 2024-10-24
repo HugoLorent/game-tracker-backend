178d3acf00e388c1ed94907b763fb954d897d85d
Hugo LORENT <hugo.lorent@outlook.com>
2024-10-24 13:28:18 +0200

    refactor: put database config in its own file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,26 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DATABASE_HOST'),
-        port: configService.get('DATABASE_PORT'),
-        username: configService.get('DATABASE_USER'),
-        password: configService.get('DATABASE_PASSWORD'),
-        database: configService.get('DATABASE_NAME'),
-        entities: [],
-        synchronize: true,
-      }),
-    }),
-  ],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(databaseConfig())],
   controllers: [AppController],
   providers: [AppService],
 })
