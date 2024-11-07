6a2d7a8eb47142c4fa9ca2cc2dfc0640f2a6d381
Hugo LORENT <hugo.lorent@outlook.com>
2024-11-07 18:20:51 +0100

    refactor: send 200 http response code for login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 
@@ -6,6 +6,7 @@ import { LoginDto } from './dto/login.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @HttpCode(200)
   @Post('login')
   public async login(@Body() loginDto: LoginDto) {
     try {
