818697e63d13310341e22b11107b67f95d4c8f45
Hugo LORENT <hugo.lorent@outlook.com>
2024-11-08 16:42:16 +0100

    feat: add authorization guard

---
+++ src/guards/authorization.guard.ts
@@ -0,0 +1,21 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { FastifyRequest } from 'fastify';
+import { UserPayload } from './interfaces/guards.interface';
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+  public async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<FastifyRequest>();
+    const id = +(request.params as { id: number }).id;
+    const user: UserPayload = request['user'];
+    if (user.sub !== id) {
+      throw new ForbiddenException();
+    }
+    return true;
+  }
+}
