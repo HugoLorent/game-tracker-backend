55572bf09e83189328f0e39526df60043d73c276
Hugo LORENT <hugo.lorent@outlook.com>
2024-11-15 16:33:19 +0100

    test: add basic test for collection resource

--- src/collections/collections.controller.spec.ts
+++ src/collections/collections.controller.spec.ts
@@ -5,10 +5,20 @@ import { CollectionsService } from './collections.service';
 describe('CollectionsController', () => {
   let controller: CollectionsController;
 
+  const mockCollectionsService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CollectionsController],
-      providers: [CollectionsService],
+      providers: [
+        { provide: CollectionsService, useValue: mockCollectionsService },
+      ],
     }).compile();
 
     controller = module.get<CollectionsController>(CollectionsController);
--- src/collections/collections.service.spec.ts
+++ src/collections/collections.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CollectionsService } from './collections.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Collection } from './entities/collection.entity';
+import { UsersService } from '../users/users.service';
 
 describe('CollectionsService', () => {
   let service: CollectionsService;
 
+  const mockCollectionsRepository = {
+    save: jest.fn(),
+    update: jest.fn(),
+    findOne: jest.fn(),
+    remove: jest.fn(),
+  };
+
+  const mockUserService = {
+    findUserById: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CollectionsService],
+      providers: [
+        CollectionsService,
+        {
+          provide: getRepositoryToken(Collection),
+          useValue: mockCollectionsRepository,
+        },
+        { provide: UsersService, useValue: mockUserService },
+      ],
     }).compile();
 
     service = module.get<CollectionsService>(CollectionsService);
